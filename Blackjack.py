2c66db24615370d22ff951d931062af010fecdeb
Jimby Raynor <67350304+JimbyRaynor@users.noreply.github.com>
2024-03-11 12:20:39 +1100

    Add files via upload

--- Blackjack.py
+++ Blackjack.py
@@ -2,18 +2,27 @@
 
 import random
 
+Acefound = False
+
 def value(cardstring):
+    global Acefound
     if cardstring[0] in ['2','3','4','5','6','7','8','9','10']:
         return int(cardstring[0])
     elif cardstring[0] == "A":
-        return 11
+        Acefound = True  # an Ace can be 1 or 11
+        return 1  # Ace=1, at most one of the Aces can be 11, so add 10 in total() if <= 21
     else:
         return 10  # face card
 
 def total(cardlist):
+    global Acefound
+    Acefound = False
     subtotal = 0
     for i in cardlist:
         subtotal = subtotal + value(i)
+    if Acefound == True:
+       if subtotal+10 <= 21:
+           subtotal = subtotal + 10  # one Ace can be 11, so add 10
     return subtotal
         
                          
@@ -35,23 +44,68 @@ for s in Suits:
 
 random.shuffle(Cards)
 
-dealer = [Cards.pop(),Cards.pop()]
-player = [Cards.pop(),Cards.pop()]
-computer1 = [Cards.pop(),Cards.pop()]
-computer2 = [Cards.pop(),Cards.pop()]
-computer3 = [Cards.pop(),Cards.pop()]
+dealer = [Cards.pop()] # dealer only gets extra cards after
+                       # all other players "stand" or "go bust"
+player = [Cards.pop()]
+computer1 = [Cards.pop()]
+computer2 = [Cards.pop()]
+computer3 = [Cards.pop()]
 
-print("Dealer: ", dealer)
-print("total = ", total(dealer))
-print("You: ",player)
-print("total = ", total(player))
-print("Computer 1:", computer1)
-print("total = ", total(computer1))
-print("Computer 2:", computer2)
-print("total = ", total(computer2))
-print("Computer 3:", computer3)
-print("total = ", total(computer3))
+def showcards():
+  print("Dealer: ", dealer)
+  print("total = ", total(dealer))
+  print("You: ", player)
+  print("total = ", total(player))
+  print("Computer 1:", computer1)
+  print("total = ", total(computer1))
+  print("Computer 2:", computer2)
+  print("total = ", total(computer2))
+  print("Computer 3:", computer3)
+  print("total = ", total(computer3))
+
+dealcard = 'y';
+computer1out = False;
+computer2out = False;
+computer3out = False;
 
 
+def computerchoice():
+   global computer1out, computer2out, computer3out 
+   if total(computer1) <= 14:
+      computer1.append(Cards.pop())
+      if total(computer1) > 21:
+          print("Computer 1 busted")
+          computer1out = True
+   else:
+      print("Computer1 stands")
+      computer1out = True;
+   if total(computer2) <= 14:
+      computer2.append(Cards.pop())
+      if total(computer2) > 21:
+          print("Computer 2 busted")
+          computer2out = True
+   else:
+      print("Computer2 stands")
+      computer2out = True;
+   if total(computer3) <= 14:
+      computer3.append(Cards.pop())
+      if total(computer3) > 21:
+          print("Computer 3 busted")
+          computer3out = True
+   else:
+      print("Computer3 stands")
+      computer3out = True;
+      
 
-print("Remaining cards = ", len(Cards))
+
+while (dealcard == 'y') or \
+      ( (not computer1out) or (not computer2out) or (not computer2out) ):
+   computerchoice()
+   player.append(Cards.pop())
+   showcards()
+   dealcard  = input("Deal another card (y/n)?")
+
+
+print("Remaining cards in deck: ", len(Cards))
+
+print("Goodbye. Have a nice day :)")
