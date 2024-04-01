592ec56dd6b0e95e2a7a5d54ae632064e85b0895
Jimby Raynor <67350304+JimbyRaynor@users.noreply.github.com>
2024-04-01 17:17:54 +1100

    Add files via upload

--- Blackjack.py
+++ Blackjack.py
@@ -1,31 +1,42 @@
 # blackjack with 3 computer players, ONE deck :) Count cards ;)
 
+from tkinter import *
+
 import random
+import time
 
-Acefound = False
+mainwin = Tk()
+mainwin.geometry("800x400")
+button1 = Button(mainwin, text="Hello World")
+button1.place(x=170, y = 100)
 
-def value(cardstring):
-    global Acefound
-    if cardstring[0] in ['2','3','4','5','6','7','8','9','10']:
-        return int(cardstring[0])
-    elif cardstring[0] == "A":
-        Acefound = True  # an Ace can be 1 or 11
-        return 1  # Ace=1, at most one of the Aces can be 11, so add 10 in total() if <= 21
-    else:
-        return 10  # face card
 
-def total(cardlist):
-    global Acefound
-    Acefound = False
-    subtotal = 0
-    for i in cardlist:
-        subtotal = subtotal + value(i)
-    if Acefound == True:
-       if subtotal+10 <= 21:
-           subtotal = subtotal + 10  # one Ace can be 11, so add 10
-    return subtotal
-        
-                         
+xgap = 153
+
+
+# NEED to store returned pointers as GLOBAL variables
+# CANNOT use PhotoImage inside a function, otherwise
+
+player1a = Canvas(mainwin, width = 64, height = 64)
+player1aimage = PhotoImage(file="Cards\card_back.png")
+player1asprite = player1a.create_image(0,0,anchor=NW,image=player1aimage)
+player1a.place(x=120,y=0)
+player1b = Canvas(mainwin, width = 64, height = 64)
+player1bimage = PhotoImage(file="Cards\card_back.png")
+player1bsprite = player1b.create_image(0,0,anchor=NW,image=player1bimage)
+player1b.place(x=120+xgap,y=0)
+player1c = Canvas(mainwin, width = 64, height = 64)
+player1cimage = PhotoImage(file="Cards\card_back.png")
+player1csprite = player1c.create_image(0,0,anchor=NW,image=player1cimage)
+player1c.place(x=120+xgap*2,y=0)
+player1d = Canvas(mainwin, width = 64, height = 64)
+player1dimage = PhotoImage(file="Cards\card_back.png")
+player1dsprite = player1d.create_image(0,0,anchor=NW,image=player1dimage)
+player1d.place(x=120+xgap*3,y=0)
+
+
+
+player = []
 
 Nums = ["Ace"]
 
@@ -51,7 +62,92 @@ computer1 = [Cards.pop()]
 computer2 = [Cards.pop()]
 computer3 = [Cards.pop()]
 
+def cardfilename(cardstring):
+    suitstring = ""
+    if "Hearts" in cardstring:
+        suitstring = "hearts"
+    if "Clubs" in cardstring:
+        suitstring = "clubs"
+    if "Diamonds" in cardstring:
+        suitstring = "diamonds"
+    if "Spades" in cardstring:
+        suitstring = "spades"
+    numberstring = ""
+    if cardstring[0] in ['2','3','4','5','6','7','8','9']:
+        numberstring = "0"+cardstring[0]
+    elif cardstring[0] == '1':
+        numberstring = '10'
+    else: numberstring = cardstring[0] # must be A, J, Q, K
+    return "Cards\\"+"card_"+suitstring+"_"+numberstring+".png"
+
+
+# cannot use PhotoImage inside a function since the returned pointer is local, and trashed
+# So use global variables to store the returned pointer
+def timerupdate():
+    global player1aimage, player1asprite, player1bimage, player1bsprite, player1cimage, player1csprite, player1dimage, player1dsprite   
+    if len(player) > 0:
+     i = 0
+     for card in player:
+        i = i + 1
+        if i == 1:
+          player1aimage = PhotoImage(file=cardfilename(card))
+          player1a.itemconfigure(player1asprite, image=player1aimage)
+        if i == 2:
+          player1bimage = PhotoImage(file=cardfilename(card))
+          player1b.itemconfigure(player1bsprite, image=player1bimage)
+        if i == 3:
+          player1cimage = PhotoImage(file=cardfilename(card))
+          player1c.itemconfigure(player1csprite, image=player1cimage)
+        if i == 4:
+          player1dimage = PhotoImage(file=cardfilename(card))
+          player1d.itemconfigure(player1dsprite, image=player1dimage)
+     mainwin.after(2000,timerupdate)
+
+
+
+
+mainwin.after(2000,timerupdate)
+mainwin.mainloop()
+
+
+Acefound = False
+
+Money = 100
+Bet = 10
+
+
+
+def value(cardstring):
+    global Acefound
+    print(cardfilename(cardstring))
+    if cardstring[0] in ['2','3','4','5','6','7','8','9']:
+        return int(cardstring[0])
+    elif cardstring[0] == "A":
+        Acefound = True  # an Ace can be 1 or 11
+        return 1  # Ace=1, at most one of the Aces can be 11, so add 10 in total() if <= 21
+    else:
+        return 10  # face card, includes case where cardstring[0]=1, missing 0, so a ten
+
+def total(cardlist):
+    global Acefound
+    Acefound = False
+    subtotal = 0
+    for i in cardlist:
+        subtotal = subtotal + value(i)
+    if Acefound == True:
+       if subtotal+10 <= 21:
+           subtotal = subtotal + 10  # one Ace can be 11, so add 10
+    return subtotal
+        
+
+
+
+
+
 def showcards():
+  time.sleep(1)
+  print("dealing ...")
+  time.sleep(2)
   print("Dealer: ", dealer)
   print("total = ", total(dealer))
   print("You: ", player)
@@ -62,11 +158,13 @@ def showcards():
   print("total = ", total(computer2))
   print("Computer 3:", computer3)
   print("total = ", total(computer3))
+  a = input("Press Enter to continue")
 
 dealcard = 'y';
 computer1out = False;
 computer2out = False;
 computer3out = False;
+playerout = False;
 
 
 def computerchoice():
@@ -77,7 +175,6 @@ def computerchoice():
           print("Computer 1 busted")
           computer1out = True
    else:
-      print("Computer1 stands")
       computer1out = True;
    if total(computer2) <= 14:
       computer2.append(Cards.pop())
@@ -85,7 +182,6 @@ def computerchoice():
           print("Computer 2 busted")
           computer2out = True
    else:
-      print("Computer2 stands")
       computer2out = True;
    if total(computer3) <= 14:
       computer3.append(Cards.pop())
@@ -93,19 +189,60 @@ def computerchoice():
           print("Computer 3 busted")
           computer3out = True
    else:
-      print("Computer3 stands")
       computer3out = True;
       
 
 
-while (dealcard == 'y') or \
-      ( (not computer1out) or (not computer2out) or (not computer2out) ):
+while  (not playerout) or \
+       (not computer1out) or (not computer2out) or (not computer3out) :
    computerchoice()
-   player.append(Cards.pop())
+   if playerout == False:
+       player.append(Cards.pop())
    showcards()
-   dealcard  = input("Deal another card (y/n)?")
+   if total(player) > 21:
+          print("Player busted")
+          playerout = True
+   if playerout == False:
+      dealcard  = input("Deal another card (y/n)?")
+   if dealcard != 'y':
+       playerout = True
+
+# after all players have finished, it is the dealers turn
+# must hit if total <= 16
+# must stand if total >= 17
+
+
+print("")
+print("Dealers turn")
+print("")
+time.sleep(3)
+
+dealerout = False;
+dealer.append(Cards.pop())
+while (not dealerout):
+    if total(dealer) <= 16:
+        dealer.append(Cards.pop())
+    if total(dealer) >= 17:
+        dealerout = True;
+    showcards()
+
+if total(dealer) > 21:
+    print("Dealer Busted!!")
+showcards()
+    
+
+if (total(player) > 21):
+    print("player lost")
+elif (total(player) > total(dealer)) or (total(dealer) > 21):
+    print("player wins")
+elif (total(player) == total(dealer)):
+    print("Push (money returned, no loss or gain)")
+else:
+    print("Player lost")
 
 
 print("Remaining cards in deck: ", len(Cards))
+
+
 
 print("Goodbye. Have a nice day :)")
